e2007f7cf36596ac204e1bfebc9f43fd95009715
Florin Petrean <fpetrean@montran.com>
2021-10-20 19:24:52 +0300

    modified gitlab-cy

--- src/main/java/com/ds/EnergyUtilityPlatform/controller/UserController.java
+++ src/main/java/com/ds/EnergyUtilityPlatform/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.ds.EnergyUtilityPlatform.controller;
 
 import com.ds.EnergyUtilityPlatform.model.dto.DtoMapper;
+import com.ds.EnergyUtilityPlatform.model.dto.JwtResponse;
+import com.ds.EnergyUtilityPlatform.model.dto.UserDetails;
 import com.ds.EnergyUtilityPlatform.model.dto.UserDto;
 import com.ds.EnergyUtilityPlatform.model.entity.AppUser;
 import com.ds.EnergyUtilityPlatform.service.ICrudService;
 import com.ds.EnergyUtilityPlatform.service.UserService;
 import com.ds.EnergyUtilityPlatform.utils.JwtUtility;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +24,19 @@ public class UserController extends CrudController<AppUser, UserDto> {
         super(service, dtoMapper);
         this.userService = (UserService) service;
         this.jwtUtility = jwtUtility;
+    }
+
+    @PostMapping("/login")
+    public JwtResponse login(@RequestBody UserDetails userDetails) throws Exception {
+        try {
+            this.userService.login(userDetails.getUsername(), userDetails.getPassword());
+        }
+        catch (Exception e) {
+            throw new Exception("Invalid Credentials", e);
+        }
+
+        String token = jwtUtility.generateToken(userDetails);
+        return new JwtResponse(token);
     }
 
 
