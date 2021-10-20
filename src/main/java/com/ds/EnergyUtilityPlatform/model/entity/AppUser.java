b50d2b7b8eb23a7599f3111a9280eabab3d6a0fa
Florin Petrean <fpetrean@montran.com>
2021-10-20 17:44:08 +0300

    created basic entities

--- src/main/java/com/ds/EnergyUtilityPlatform/model/entity/AppUser.java
+++ src/main/java/com/ds/EnergyUtilityPlatform/model/entity/AppUser.java
@@ -20,7 +20,7 @@ import java.util.List;
 @AllArgsConstructor
 public class AppUser implements IEntity<AppUser>{
     @Id
-    @Column(name = "id", nullable = false)
+    @Column(nullable = false)
     private Long id;
 
     @Column
