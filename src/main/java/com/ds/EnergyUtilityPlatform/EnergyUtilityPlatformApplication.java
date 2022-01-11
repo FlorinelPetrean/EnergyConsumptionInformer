d3fb16a7605999185e2f87d6d79db48da8fdc080
Florin Petrean <fpetrean@montran.com>
2022-01-12 01:49:50 +0200

    added time zone UTC

--- src/main/java/com/ds/EnergyUtilityPlatform/EnergyUtilityPlatformApplication.java
+++ src/main/java/com/ds/EnergyUtilityPlatform/EnergyUtilityPlatformApplication.java
@@ -3,10 +3,13 @@ package com.ds.EnergyUtilityPlatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EnergyUtilityPlatformApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(EnergyUtilityPlatformApplication.class, args);
 	}
 
