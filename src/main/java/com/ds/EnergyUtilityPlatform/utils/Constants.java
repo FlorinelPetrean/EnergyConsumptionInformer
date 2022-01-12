9fe35ce46078c6b078123fc81ab57ff3547fa1df
Florin Petrean <fpetrean@montran.com>
2022-01-12 03:21:03 +0200

    for production

--- src/main/java/com/ds/EnergyUtilityPlatform/utils/Constants.java
+++ src/main/java/com/ds/EnergyUtilityPlatform/utils/Constants.java
@@ -1,6 +1,6 @@
 package com.ds.EnergyUtilityPlatform.utils;
 
 public class Constants {
-    public static final String frontend = "http://localhost:3000";
-//    public static final String frontend = "https://ds2021frontend-eup.herokuapp.com";
+//    public static final String frontend = "http://localhost:3000";
+    public static final String frontend = "https://ds2021frontend-eup.herokuapp.com";
 }
